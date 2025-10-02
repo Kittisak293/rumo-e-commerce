b0fa16167fe5f2247caab85bb2011d0581c4d736
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-03 01:00:24 +0700

    feat(users): add users module and install typeORM, swagger

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  passwordHash: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['admin', 'customer'],
+    default: 'customer',
+  })
+  role: 'admin' | 'customer';
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+}
