2feff9d9ac96c215076e9e9526be9f34c435785f
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-03 01:07:37 +0700

    chore: install class-tranformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 export class CreateUserDto {
-    @ApiProperty({
+  @ApiProperty({
     description: 'อีเมลสำหรับเข้าสู่ระบบ',
     example: 'user@example.com',
   })
+  @IsNotEmpty()
   @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'ชื่อผู้ใช้',
+    example: 'Kittisak',
+  })
+  @IsNotEmpty()
+  username: string;
+
+  @ApiProperty({
+    description: 'รหัสผ่าน (ต้องมีความยาว 8-32 ตัวอักษร)',
+    example: 'Kittisak123456789',
+    minLength: 8,
+    maxLength: 32,
+  })
+  @IsNotEmpty()
+  @Length(8, 32)
+  password: string;
+
+  @ApiProperty({
+    description: 'ชุด id ของ ผู้ใช้',
+    example: [1, 2],
+  })
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsNotEmpty()
+  roleIds: number;
 }
