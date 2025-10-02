50e410c4ba0540c5a81b0bcdc4f43044f90eec54
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-03 01:02:54 +0700

    chore: install class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+} from 'class-validator';
 export class CreateUserDto {
     @ApiProperty({
-
-    })
+    description: 'อีเมลสำหรับเข้าสู่ระบบ',
+    example: 'user@example.com',
+  })
+  @IsEmail()
 }
