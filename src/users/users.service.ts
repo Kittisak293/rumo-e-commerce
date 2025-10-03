bc436b188575066eec830cab950cda7fb0cac5a3
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-03 21:51:17 +0700

    chore: install bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,6 +13,7 @@ export class UsersService {
     private readonly usersRepository: Repository<User>,
   ) {}
   create(createUserDto: CreateUserDto) {
+    const passwordHash = await bcrypt.hash;
     return this.usersRepository.save(createUserDto);
   }
 
