ed84fa5dcc767a466524673d13649d8b0c81c856
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-06 02:25:43 +0700

    feat(products): add product DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,36 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({ description: 'ชื่อสินค้า', example: 'Latte' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: 'รายละเอียดสิค้า',
+    example:
+      'เมาส์เกมมิ่ง สามารถเชื่อมต่อได้ทั้ง Bluetooth และ USB สามารถใช้ได้ทั้ง Windows, macOS และ Linux',
+  })
+  description: string;
+
+  @ApiProperty({ description: 'ราคาสินค้า', example: 1590, minimum: 0 })
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @ApiProperty({ description: 'จำนวนสินค้าในคลัง', example: 62, minimum: 0 })
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @Min(0)
+  stock: number;
+
+  @ApiProperty({ description: 'id ของ category', example: 1, minimum: 1 })
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @Min(1)
+  categoryId: number;
+
+  //ยังไม่เสร็จยังงงตรง category อยู่
+}
