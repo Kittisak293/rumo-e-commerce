742b785c86cab3e642bd442b6cce9ea1a2c3aa65
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-08 23:55:47 +0700

    feat(category): create DTO

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @ApiProperty({ description: 'ชื่อหมวดหมู่', example: 'clothes' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: 'ส่วนท้ายของ URL ที่ใช้ระบุหมวดหมู่ในเว็บไซต์',
+    example: 'mens-clothing',
+  })
+  @IsString()
+  slug: string;
+}
