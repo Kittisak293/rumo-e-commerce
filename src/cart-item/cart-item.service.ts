b80ca9a2b0ec1790b630ff16ac2188f99368eb06
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-10 03:10:28 +0700

    feat(cart-item): add cart-item service

--- src/cart-item/cart-item.service.ts
+++ src/cart-item/cart-item.service.ts
@@ -1,26 +1,83 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCartItemDto } from './dto/create-cart-item.dto';
 import { UpdateCartItemDto } from './dto/update-cart-item.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CartItem } from './entities/cart-item.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Product } from 'src/products/entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CartItemService {
-  create(createCartItemDto: CreateCartItemDto) {
-    return 'This action adds a new cartItem';
+  constructor(
+    @InjectRepository(CartItem)
+    private readonly cartItemsRepository: Repository<CartItem>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createCartItemDto: CreateCartItemDto) {
+    const user = await this.usersRepository.findOneByOrFail({
+      id: createCartItemDto.userId,
+    });
+
+    const product = await this.productsRepository.findOneByOrFail({
+      id: createCartItemDto.productId,
+    });
+
+    const existingItem = await this.cartItemsRepository.findOne({
+      where: {
+        user: { id: user.id },
+        product: { id: product.id },
+      },
+      relations: ['product', 'user'],
+    });
+
+    if (existingItem) {
+      existingItem.quantity += createCartItemDto.quantity;
+      return this.cartItemsRepository.save(existingItem);
+    }
+
+    const item = this.cartItemsRepository.create({
+      ...createCartItemDto,
+      user,
+      product,
+    });
+
+    return await this.cartItemsRepository.save(item);
   }
 
-  findAll() {
-    return `This action returns all cartItem`;
+  async findByUserId(userId: number): Promise<CartItem[]> {
+    return await this.cartItemsRepository.find({
+      where: { user: { id: userId } },
+      relations: ['product', 'user'],
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cartItem`;
+  async findAll() {
+    return await this.cartItemsRepository.find();
   }
 
-  update(id: number, updateCartItemDto: UpdateCartItemDto) {
-    return `This action updates a #${id} cartItem`;
+  async findOne(id: number) {
+    return await this.cartItemsRepository.findOne({ where: { id: id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cartItem`;
+  async update(id: number, updateCartItemDto: UpdateCartItemDto) {
+    const item = await this.cartItemsRepository.findOneByOrFail({ id });
+
+    item.quantity = updateCartItemDto.quantity ?? item.quantity;
+    item.price = updateCartItemDto.price ?? item.price;
+
+    return await this.cartItemsRepository.save(item);
+  }
+
+  async remove(id: number) {
+    const item = await this.cartItemsRepository.findOneByOrFail({ id });
+    await this.cartItemsRepository.remove(item);
+    return item;
   }
 }
