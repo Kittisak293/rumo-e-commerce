a20e9405388d243671d936b2e52fedb65a0039e7
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-12 18:55:02 +0700

    feat(carriers): create carrier service

--- src/carriers/carriers.service.ts
+++ src/carriers/carriers.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCarrierDto } from './dto/create-carrier.dto';
 import { UpdateCarrierDto } from './dto/update-carrier.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Carrier } from './entities/carrier.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CarriersService {
-  create(createCarrierDto: CreateCarrierDto) {
-    return 'This action adds a new carrier';
+  constructor(
+    @InjectRepository(Carrier)
+    private readonly carriersRepo: Repository<Carrier>,
+  ) {}
+
+  async create(createCarrierDto: CreateCarrierDto) {
+    return await this.carriersRepo.save(createCarrierDto);
   }
 
-  findAll() {
-    return `This action returns all carriers`;
+  async findAll() {
+    return await this.carriersRepo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} carrier`;
+  async findOne(id: number) {
+    return await this.carriersRepo.findOne({ where: { id: id } });
   }
 
-  update(id: number, updateCarrierDto: UpdateCarrierDto) {
-    return `This action updates a #${id} carrier`;
+  async update(id: number, updateCarrierDto: UpdateCarrierDto) {
+    return await this.carriersRepo.update(id, updateCarrierDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} carrier`;
+  async remove(id: number) {
+    const carrier = await this.carriersRepo.findOne({
+      where: { id: id },
+    });
+    await this.carriersRepo.softDelete(id);
+    return carrier;
   }
 }
