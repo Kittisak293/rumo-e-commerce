cb1b5a633f9b4ac689bfacaff05312f445661128
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-10-12 17:07:32 +0700

    feat(shipment_events): add shipment_event service

--- src/shipment_events/shipment_events.service.ts
+++ src/shipment_events/shipment_events.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { CreateShipmentEventDto } from './dto/create-shipment_event.dto';
 import { UpdateShipmentEventDto } from './dto/update-shipment_event.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ShipmentEvent } from './entities/shipment_event.entity';
+import { Shipment } from 'src/shipments/entities/shipment.entity';
 
 @Injectable()
 export class ShipmentEventsService {
-  create(createShipmentEventDto: CreateShipmentEventDto) {
-    return 'This action adds a new shipmentEvent';
+  constructor(
+    @InjectRepository(ShipmentEvent)
+    private readonly shipmentEventsRepo: Repository<ShipmentEvent>,
+    @InjectRepository(Shipment)
+    private readonly shipmentsRepo: Repository<Shipment>,
+  ) {}
+
+  async create(createShipmentEventDto: CreateShipmentEventDto) {
+    const shipmentEvent = this.shipmentEventsRepo.create(
+      createShipmentEventDto,
+    );
+    const shipment = await this.shipmentsRepo.findOneByOrFail({
+      id: createShipmentEventDto.shipmentId,
+    });
+
+    shipmentEvent.shipment = shipment;
+
+    return await this.shipmentEventsRepo.save(shipmentEvent);
   }
 
-  findAll() {
-    return `This action returns all shipmentEvents`;
+  async findAll() {
+    return await this.shipmentEventsRepo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} shipmentEvent`;
+  async findOne(id: number) {
+    return await this.shipmentEventsRepo.findOne({ where: { id: id } });
   }
 
-  update(id: number, updateShipmentEventDto: UpdateShipmentEventDto) {
-    return `This action updates a #${id} shipmentEvent`;
+  async update(id: number, updateShipmentEventDto: UpdateShipmentEventDto) {
+    const shipmentEvent = await this.shipmentEventsRepo.findOneOrFail({
+      where: { id: id },
+    });
+    const shipment = await this.shipmentsRepo.findOneByOrFail({
+      id: updateShipmentEventDto.shipmentId,
+    });
+
+    shipmentEvent.shipment = shipment;
+
+    return await this.shipmentEventsRepo.save(shipmentEvent);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} shipmentEvent`;
+  async remove(id: number) {
+    const shipmentEvent = await this.shipmentEventsRepo.findOneOrFail({
+      where: { id: id },
+    });
+
+    await this.shipmentEventsRepo.softDelete(id);
+    return shipmentEvent;
   }
 }
