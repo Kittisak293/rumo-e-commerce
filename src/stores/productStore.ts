4b1d1335a9c8262d5faaf70b78870769b38d3836
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-20 17:46:46 +0700

    refactor(productStore): change structure

--- src/stores/productStore.ts
+++ src/stores/productStore.ts
@@ -8,36 +8,17 @@ export const useProductStore = defineStore('product', () => {
   const products = ref<Product[]>([]);
 
   async function addProduct(p: Product, file: File | null) {
-    try {
-      Loading.show();
-      const formData = new FormData();
-      formData.append('name', String(p.name));
-      formData.append('description', String(p.description));
-      formData.append('price', String(p.price));
-      formData.append('stock', String(p.stock));
-      formData.append('categoryId', String(p.categoryId));
-      formData.append('ratingAvg', String(p.ratingAvg));
-      formData.append('ratingCount', String(p.ratingCount));
-      formData.append('soldCount', String(p.soldCount));
-      if (file) {
-        formData.append('file', file);
-      }
-      const res = await api.post('/products', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-      console.log(res.data);
-      await getProducts();
-    } catch (err) {
-      console.error(err);
-      Notify.create({
-        color: 'negative',
-        position: 'top',
-        message: 'Add failed',
-        icon: 'report_problem',
-      });
-    } finally {
-      // Loading.hide()
+    const formData = new FormData();
+    formData.append('name', p.name);
+    formData.append('description', p.description);
+    formData.append('price', String(p.price));
+    formData.append('stock', String(p.stock));
+    formData.append('categoryId', String(p.categoryId));
+    if (file) {
+      formData.append('file', file);
     }
+    await api.post('/products', formData);
+    await getProducts();
   }
 
   async function delProduct(p: Product) {
@@ -60,38 +41,18 @@ export const useProductStore = defineStore('product', () => {
     }
   }
 
-  async function updateProduct(p: Product, file: File | null) {
-    try {
-      Loading.show();
-      const formData = new FormData();
-      formData.append('name', String(p.name));
-      formData.append('description', String(p.description));
-      formData.append('price', String(p.price));
-      formData.append('stock', String(p.stock));
-      formData.append('categoryId', String(p.categoryId));
-      formData.append('ratingAvg', String(p.ratingAvg));
-      formData.append('ratingCount', String(p.ratingCount));
-      formData.append('soldCount', String(p.soldCount));
-      if (file) {
-        formData.append('file', file);
-      }
-      const res = await api.patch('/products/' + p.id, formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-      console.log(res.data);
-      await getProducts();
-    } catch (err) {
-      console.error(err);
-      Notify.create({
-        color: 'negative',
-        position: 'top',
-        message: 'Update failed',
-        icon: 'report_problem',
-      });
-    } finally {
-      console.log('finally');
-      Loading.hide();
+  async function updateProduct(id: number, p: Product, file: File | null) {
+    const formData = new FormData();
+    formData.append('name', p.name);
+    formData.append('description', p.description);
+    formData.append('price', String(p.price));
+    formData.append('stock', String(p.stock));
+    formData.append('categoryId', String(p.categoryId));
+    if (file) {
+      formData.append('file', file);
     }
+    await api.patch(`/products/${id}`, formData);
+    await getProducts();
   }
 
   async function getProducts() {
