5a2b34de5318d70db6e7257e830e845b95aacf89
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-20 16:53:15 +0700

    feat(models): create models

---
+++ src/models.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number
+  name: string
+  description: string
+  price: number
+  stock: number
+  categoryId: number
+  ratingAvg: number
+  ratingCount: number
+  soldCount: number
+}
