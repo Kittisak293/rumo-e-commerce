03f8de77f9a52057c95dd35d8fc5e0f432e8924a
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-20 15:38:07 +0700

    feat(api): add axios configuration and global api instance

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,16 @@
 <template>
   <q-page padding>
     <div>First Page</div>
+    <ProductCard
+      :image="ShoeImg"
+      name="ADIDAS Adizero EVO SL"
+      :price="5800"
+      :sold="53"
+      :rating="4.9"
+    />
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProductCard from 'src/components/ProductCard.vue';
+</script>
