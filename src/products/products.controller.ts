2833f3089dd509b7844f2aa35256e8cb3761a354
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-20 18:13:27 +0700

    feat: add multer and platform-express for file upload support

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   Controller,
   Get,
@@ -6,18 +7,46 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody } from '@nestjs/swagger';
+import { extname } from 'path';
+import { v4 } from 'uuid';
+import { diskStorage } from 'multer';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @ApiBody({ description: 'ข้อมูลสินค้า', type: CreateProductDto })
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/products',
+        filename: (req, file, callback) => {
+          console.log(file);
+          const uniqueFileName = v4() + extname(file.originalname);
+          callback(null, uniqueFileName);
+        },
+      }),
+    }),
+  )
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createProductDto: CreateProductDto,
+  ) {
+    return this.productsService.create({
+      ...createProductDto,
+      imageUrl: file
+        ? '/product-images/' + file.filename
+        : '/product-images/unknown.jpg',
+    });
   }
 
   @Get()
