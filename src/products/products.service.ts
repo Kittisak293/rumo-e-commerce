08b62786c546d88044d49a2714f4fad4041588a1
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-23 12:47:51 +0700

    feat(Product): add searchHomeProducts in Product Service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -114,4 +114,51 @@ export class ProductsService {
     }
     return qb.getMany();
   }
+
+  async searchHomeProducts(filters: {
+    sortBy?: string;
+    storeType?: 'mall' | 'seller';
+    priceMin?: number;
+    priceMax?: number;
+    ratingMin?: number;
+  }) {
+    const qb = this.productsRepository.createQueryBuilder('p');
+    // ประเภทร้าน
+    if (filters.storeType) {
+      qb.andWhere('p.storeType = :storeType', { storeType: filters.storeType });
+    }
+
+    // ราคา
+    if (filters.priceMin !== null && filters.priceMin !== undefined) {
+      qb.andWhere('p.price >= :min', { min: filters.priceMin });
+    }
+
+    if (filters.priceMax !== null && filters.priceMax !== undefined) {
+      qb.andWhere('p.price <= :max', { max: filters.priceMax });
+    }
+
+    // คะแนน
+    if (filters.ratingMin !== null && filters.ratingMin !== undefined) {
+      qb.andWhere('p.ratingAvg >= :rating', { rating: filters.ratingMin });
+    }
+
+    // จัดเรียง
+    switch (filters.sortBy) {
+      case 'popular':
+        qb.orderBy('p.sold', 'DESC');
+        break;
+      case 'latest':
+        qb.orderBy('p.createdAt', 'DESC');
+        break;
+      case 'priceAsc':
+        qb.orderBy('p.price', 'ASC');
+        break;
+      case 'priceDesc':
+        qb.orderBy('p.price', 'DESC');
+        break;
+      default:
+        qb.orderBy('p.id', 'DESC');
+    }
+    return qb.getMany();
+  }
 }
