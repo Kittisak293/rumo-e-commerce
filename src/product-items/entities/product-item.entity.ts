43dcedc009206311aade5fdfd355280db37af1ec
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-27 22:52:07 +0700

    feat(product-items): add Entities

--- src/product-items/entities/product-item.entity.ts
+++ src/product-items/entities/product-item.entity.ts
@@ -1,1 +1,16 @@
-export class ProductItem {}
+import { Product } from 'src/products/entities/product.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class ProductImage {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Product, (product) => product.images, {
+    onDelete: 'CASCADE',
+  })
+  product: Product;
+
+  @Column()
+  imageUrl: string;
+}
