8a3a1056802b93f9f48501cf92926119e88a30d8
Kittisak Janwanrak <66160293@go.buu.ac.th>
2025-11-27 22:56:11 +0700

    fix(product-items): Remove incorrectly generated 'product-items' resource

--- src/product-items/product-items.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductItemsService } from './product-items.service';
-import { ProductItemsController } from './product-items.controller';
-
-@Module({
-  controllers: [ProductItemsController],
-  providers: [ProductItemsService],
-})
-export class ProductItemsModule {}
